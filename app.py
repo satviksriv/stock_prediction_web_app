fac544ac5b19d34d583dc6a184a2129ed3f4dceb
satviksriv <srivastavasatvik22@gmail.com>
2022-05-16 15:15:14 +0530

    finalize

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from sklearn.preprocessing import MinMaxScaler
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -13,21 +14,21 @@ st.title('Stock Trend Prediction')
 user_input = st.text_input('Enter Stock Ticker', 'AAPL')
 df = data.DataReader(user_input, 'yahoo', start, end)
 
-# Describing Data
+# * Describing Data
 
 st.subheader('Data from 2010 - 2019')
 st.write(df.describe())
 
-# Visualization
+# * Visualization
 
 st.subheader('Closing Price vs Time chart')
-fig = plt.figure(figsize= (12, 6))
+fig = plt.figure(figsize=(12, 6))
 plt.plot(df.Close)
 st.pyplot(fig)
 
 st.subheader('Closing Price vs Time chart with 100MA')
 ma100 = df.Close.rolling(100).mean()
-fig = plt.figure(figsize= (12, 6))
+fig = plt.figure(figsize=(12, 6))
 plt.plot(ma100, 'r')
 plt.plot(df.Close, 'b')
 st.pyplot(fig)
@@ -35,30 +36,29 @@ st.pyplot(fig)
 st.subheader('Closing Price vs Time chart with 100MA & 200MA')
 ma100 = df.Close.rolling(100).mean()
 ma200 = df.Close.rolling(200).mean()
-fig = plt.figure(figsize= (12, 6))
+fig = plt.figure(figsize=(12, 6))
 plt.plot(ma100, 'r')
 plt.plot(ma200, 'g')
 plt.plot(df.Close, 'b')
 st.pyplot(fig)
 
-# Splitting data into Training and Testing
+# * Splitting data into Training and Testing
 
 data_training = pd.DataFrame(df['Close'][0:int(len(df)*0.70)])
 data_testing = pd.DataFrame(df['Close'][int(len(df)*0.70):int(len(df))])
 
-from sklearn.preprocessing import MinMaxScaler
-scaler = MinMaxScaler(feature_range=(0,1))
+scaler = MinMaxScaler(feature_range=(0, 1))
 
 data_training_array = scaler.fit_transform(data_training)
 
-# Load Model
+# * Load Model
 
 model = load_model('keras_model.h5')
 
-# Testing Part
+# * Testing Part
 
 past_100_days = data_training.tail(100)
-final_df = past_100_days.append(data_testing, ignore_index= True)
+final_df = past_100_days.append(data_testing, ignore_index=True)
 input_data = scaler.fit_transform(final_df)
 
 x_test = []
@@ -76,12 +76,12 @@ scale_factor = 1/scaler[0]
 y_predicted = y_predicted * scale_factor
 y_test = y_test * scale_factor
 
-# Final Graph
+# * Final Graph
 
 st.subheader('Predictions vs Original')
-fig2 = plt.figure(figsize=(12,6))
-plt.plot(y_test, 'b', label= 'Original Price')
-plt.plot(y_predicted, 'r', label = 'Predicted Price')
+fig2 = plt.figure(figsize=(12, 6))
+plt.plot(y_test, 'b', label='Original Price')
+plt.plot(y_predicted, 'r', label='Predicted Price')
 plt.xlabel('Time')
 plt.ylabel('Price')
 plt.legend()
